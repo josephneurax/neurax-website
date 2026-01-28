18d686b1f8b6b7ba1e16d580b638f65e04ffda7f
Yochka Ephraim Joseph <yochkaephraimjoseph@icloud.com>
2026-01-27 19:08:03 -0500

    Add handleOpenQuestionnaire function to App component

--- App.tsx
+++ App.tsx
@@ -4,9 +4,14 @@ import { Hero3D } from './Hero3D'
 import { Header } from './Header'
 
 export default function App() {
+  const handleOpenQuestionnaire = (service?: 'website' | 'vocal') => {
+    console.log('Questionnaire ouvert pour:', service);
+    // Logique future pour le questionnaire
+  };
+
   return (
     <div className="min-h-screen bg-black text-white">
-      <Header />
+      <Header onOpenQuestionnaire={handleOpenQuestionnaire} />
       <Hero3D />
       <InteractiveChatDemo />
     </div>
