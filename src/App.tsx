f13edfe2ff9b469bcd8c5f5405858f8c2f09d216
Yochka Ephraim Joseph <yochkaephraimjoseph@icloud.com>
2026-01-27 19:36:00 -0500

    Create App component with header and demos

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { InteractiveChatDemo } from './InteractiveChatDemo'
+import { Hero3D } from './Hero3D'
+import { Header } from './Header'
+
+export default function App() {
+  const handleOpenQuestionnaire = (service?: 'website' | 'vocal') => {
+    console.log('Questionnaire ouvert pour:', service);
+  };
+
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <Header onOpenQuestionnaire={handleOpenQuestionnaire} />
+      <Hero3D />
+      <InteractiveChatDemo />
+    </div>
+  )
+}
